f5d964853db99a8d6dc69137cc08267d2e38f857
jD91mZM2 <me@krake.one>
2017-12-12 08:04:08 +0100

    Fix compilation error

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,9 @@
-use native_tls::Error as TlsError;
-use native_tls::HandshakeError as TlsHandshakeError;
+#[cfg(feature = "native-tls")] use native_tls::Error as TlsError;
+#[cfg(feature = "native-tls")] use native_tls::HandshakeError as TlsHandshakeError;
+#[cfg(feature = "native-tls")] use std::net::TcpStream;
 use std::error::Error as StdError;
 use std::fmt;
 use std::io::Error as IoError;
-use std::net::TcpStream;
 use std::num::ParseIntError;
 
 /// minttp error type
@@ -13,7 +13,9 @@ pub enum Error {
 	InvalidStatusLine,
     IoError(IoError),
     ParseIntError(ParseIntError),
+    #[cfg(feature = "native-tls")]
     TlsError(TlsError),
+    #[cfg(feature = "native-tls")]
     TlsHandshakeError(TlsHandshakeError<TcpStream>),
 }
 
@@ -24,7 +26,9 @@ impl fmt::Display for Error {
             Error::InvalidStatusLine   => write!(f, "{}", self.description()),
             Error::IoError(ref inner)  => write!(f, "{}", inner),
             Error::ParseIntError(ref inner) => write!(f, "{}", inner),
+            #[cfg(feature = "native-tls")]
             Error::TlsError(ref inner) => write!(f, "{}", inner),
+            #[cfg(feature = "native-tls")]
             Error::TlsHandshakeError(ref inner) => write!(f, "{}", inner)
         }
     }
@@ -36,7 +40,9 @@ impl StdError for Error {
             Error::InvalidStatusLine   => "Response parsing error: Invalid status line",
             Error::IoError(ref inner)  => inner.description(),
             Error::ParseIntError(ref inner) => inner.description(),
+            #[cfg(feature = "native-tls")]
             Error::TlsError(ref inner) => inner.description(),
+            #[cfg(feature = "native-tls")]
             Error::TlsHandshakeError(ref inner) => inner.description()
         }
     }
@@ -44,21 +50,25 @@ impl StdError for Error {
         match *self {
             Error::IoError(ref inner)  => Some(inner),
             Error::ParseIntError(ref inner) => Some(inner),
+            #[cfg(feature = "native-tls")]
             Error::TlsError(ref inner) => Some(inner),
+            #[cfg(feature = "native-tls")]
             Error::TlsHandshakeError(ref inner) => Some(inner),
             _ => None
         }
     }
 }
-impl From<TlsError> for Error {
-    fn from(error: TlsError) -> Self { Error::TlsError(error) }
-}
 impl From<IoError> for Error {
     fn from(error: IoError) -> Self { Error::IoError(error) }
 }
-impl From<TlsHandshakeError<TcpStream>> for Error {
-    fn from(error: TlsHandshakeError<TcpStream>) -> Self { Error::TlsHandshakeError(error) }
-}
 impl From<ParseIntError> for Error {
     fn from(error: ParseIntError) -> Self { Error::ParseIntError(error) }
+}
+#[cfg(feature = "native-tls")]
+impl From<TlsError> for Error {
+    fn from(error: TlsError) -> Self { Error::TlsError(error) }
+}
+#[cfg(feature = "native-tls")]
+impl From<TlsHandshakeError<TcpStream>> for Error {
+    fn from(error: TlsHandshakeError<TcpStream>) -> Self { Error::TlsHandshakeError(error) }
 }
